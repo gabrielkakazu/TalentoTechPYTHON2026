51291e5a775f4a4a2b4e3a0d7b8ffd850498b2a5
Gabriel Kakazu <142187006+gabrielkakazu@users.noreply.github.com>
2026-04-26 22:55:04 -0300

    diccionario con validación

--- clase8.py
+++ clase8.py
@@ -7,24 +7,57 @@
 productos = {}
 
 while True:
+    finalizar = input('Escriba "fin" para finalizar la carga de productos: ')
+    if finalizar.lower().strip() == "fin":
+        print("Gracias, vuelva pronto")
+        break
+
+    nombre = input("Ingrese nombre de producto: ").strip()
+
+    while nombre == "":
+        print("[ALERTA] ¡El producto no puede tener un nombre vacío!")
+        nombre = input("Vuelva a ingresar nombre: ").strip()
+
+    # Validación de precio
+    while True:
+        precio = input("Ingrese precio de producto: ")
+        try:
+            precio = float(precio)
+            if precio <= 0:
+                print("[ALERTA] ¡El precio debe ser mayor que 0!")
+            else:
+                break
+        except ValueError:
+            print("[ALERTA] ¡El precio debe ser un número válido!")
+
+    productos[nombre] = precio
+    print(f"Diccionario actual: {productos}")
+
+
+"""
+productos = {}
+
+while True:
     finalizar = input("Escriba \"fin\" para finalizar la carga de productos ")
     if finalizar.lower().strip() == "fin":
         print("Gracias, vuelva pronto")
         break
     
     nombre = input("Ingrese nombre de producto: ")
-    precio = int(input("Ingrese precio de producto: "))
+    precio = input("Ingrese precio de producto: ")
 
     while nombre == "":
         print("[ALERTA] ¡El producto no puede tener un nombre vacío!")
         nombre = input("Vuelva a ingresar nombre: ")
 
-    while precio <= 0:
-        print("[ALERTA] ¡El precio no puede ser 0 o negativo!")
-        precio = int(input("Vuelva a ingresar precio :"))
-        
+    while not precio.isdigit():
+        print("[ALERTA] ¡El precio tiene ser en números!")
+        precio = input("Vuelva a ingresar precio :")
+        if precio.isdigit() and float(precio) <= 0:
+            print("[ALERTA] ¡El precio tiene ser en números!")
+            precio = input("Vuelva a ingresar precio :")
 
-    productos.setdefault(nombre, precio)
+    productos.setdefault(nombre, float(precio))
     print(f"Este es el diccionario con {productos}")
-
+"""
 
