86936e9c552fe486aff695513ab2d61c4048bfcf
Gabriel Kakazu <142187006+gabrielkakazu@users.noreply.github.com>
2026-04-26 22:35:22 -0300

    Ej8 diccionario completo

--- clase8.py
+++ clase8.py
@@ -1,6 +1,5 @@
 """
-* Crear un diccionario llamado productos donde las claves 
-sean los nombres de los productos y los valores sean sus precios.
+* Crear un diccionario llamado productos donde las claves sean los nombres de los productos y los valores sean sus precios.
 * Permitir que se agreguen productos y sus precios hasta que se decida finalizar.
 * Mostrar el contenido del diccionario después de cada operación.
 """
@@ -8,11 +7,21 @@ sean los nombres de los productos y los valores sean sus precios.
 productos = {}
 
 while True:
-    finalizar = input("Escriba \"fin\" para finalizar la carga")
+    finalizar = input("Escriba \"fin\" para finalizar la carga de productos ")
     if finalizar.lower().strip() == "fin":
         print("Gracias, vuelva pronto")
         break
-    else:
-        print(productos)
-        # acá falta todo
+    
+    nombre = input("Ingrese nombre de producto: ")
+    precio = int(input("Ingrese precio de producto: "))
+
+    while nombre == "":
+        nombre = input("[ALERTA] ¡El producto no puede tener un nombre vacío!")
+
+    while precio <= 0:
+        precio = int(input("[ALERTA] ¡El precio no puede ser 0 o negativo!"))
+
+    productos.setdefault(nombre, precio)
+    print(f"Este es el diccionario con {productos}")
+
 
