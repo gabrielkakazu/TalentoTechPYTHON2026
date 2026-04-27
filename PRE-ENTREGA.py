158f61d53988c41eb3c5e6c9ecf866701ce1f0ce
Gabriel Kakazu <142187006+gabrielkakazu@users.noreply.github.com>
2026-04-27 13:16:30 -0300

    preentrega completa

--- PRE-ENTREGA.py
+++ PRE-ENTREGA.py
@@ -1,41 +1,100 @@
-pantalla = "Sistema de Gestión Básica de Productos \n 1. Agregar producto \n 2. Mostrar productos \n 3. Buscar producto \n 4. Eliminar producto \n 5. Salir"
+# MENU DE OPCIONES
+pantalla = "Sistema de Gestión Básica de Productos"
 print (pantalla)
 
 productos = {}
 
 while True:
-    menu = input("Ingrese opción del 1 al 5 :")
+    
+    menu = input(" 1. Ingresar productos\n 2. Mostrar productos\n 3. Buscar producto\n 4. Eliminar producto\n 5. Salir\nIngrese opción del 1 al 5 :")
+    # VALIDACION DE OPCIONES
     if not menu.isdigit() or int(menu) < 1 or int(menu) > 5:
         print("No ingresaste una opción válida! ")
         continue
 
     match int(menu):
+        # OPCION SALIDA DE MENU
         case 5:
             print("Gracias, vuelvas pronto")
+            print("Menu desarrollado por @GabrielKakazu")
+            print("Talento Tech 2026 - Iniciación a Python")
             break
+
+        # OPCION MOSTRAR PRODUCTOS
         case 2:
-            print(f"Productos : {productos}")
+            print("Tenemos los siguientes productos: ")
+            contador = 1
+            for nombre in productos:
+                print(f"Prod nro {contador}: {nombre} a un precio de ${productos[nombre]}")
+                contador += 1
+            print("Volviendo a Menú Inicial...")
+
+        # OPCION INGRESAR PRODUCTOS
         case 1:
-            nombre = input("Ingrese nombre de producto: ").strip()
-
-            while nombre == "":
-                print("[ALERTA] ¡El producto no puede tener un nombre vacío!")
-                nombre = input("Vuelva a ingresar nombre: ").strip()
-
-            # Validación de precio
             while True:
-                precio = input("Ingrese precio de producto: ")
-                try:
-                    precio = float(precio)
-                    if precio <= 0:
-                        print("[ALERTA] ¡El precio debe ser mayor que 0!")
-                    else:
-                        break
-                except ValueError:
-                    print("[ALERTA] ¡El precio debe ser un número válido!")
+                nombre = input("Ingrese nombre de producto: ").strip()
+                
+                # VALIDACION DE NOMBRE DE PRODUCTO NO VACÍO 
+                while nombre == "":
+                    print("[ALERTA] ¡El producto no puede tener un nombre vacío!")
+                    nombre = input("Vuelva a ingresar nombre: ").strip()
 
-            productos[nombre] = precio
-            print(f"Diccionario actual: {productos}")
+                # Validación de precio
+                while True:
+                    precio = input("Ingrese precio de producto: ")
+                    try:
+                        precio = float(precio)
+                        if precio <= 0:
+                            print("[ALERTA] ¡El precio debe ser mayor que 0!")
+                        else:
+                            break
+                    except ValueError:
+                        print("[ALERTA] ¡El precio debe ser un número válido!")
+
+                productos[nombre] = precio
+                print(f"Productos actuales: {productos}")
+
+                salir = input("Ingrese cualquier tecla ó 's' para volver a Menu Inicial ")
+                if salir.lower().strip() == "s":
+                    print("Volviendo a Menú Inicial...")
+                    break   
+
+        # BUSQUEDA DE PRODUCTOS
+        case 3:
+            while True:
+                productoBuscado = input("Ingrese nombre de producto: ")
+                if productoBuscado not in productos:
+                    print("Lo siento, el producto no esta disponible... ")
+                    
+                else:
+                    print(f"Aquí tenemos {productoBuscado} a un precio de ${productos.get(productoBuscado)}")
+                
+                salir = input("Presione 's' para volver a Menu Inicial ")
+                if salir.lower().strip() == "s":
+                    print("Volviendo a Menú Inicial...")
+                    break
+        
+        # ELIMINAR PRODUCTOS
+        case 4:
+                     
+            while True:
+                productoABorrar = input("Ingrese nombre de producto a eliminar: ")
+                if productoABorrar.lower().strip() not in productos:
+                        print("Lo siento, el producto no existe en nuestra BD")
+                else:
+                    print(f"Eliminamos {productoABorrar} de nuestra BD")
+                    del productos[productoABorrar]
+
+                salir = input("Presione S para volver al Menu Inicial :")
+                if salir.lower().strip() == "s":
+                    print("Volviendo a Menú Inicial")
+                    break
+                
+                    
+
+
+        
+
 
 
 
