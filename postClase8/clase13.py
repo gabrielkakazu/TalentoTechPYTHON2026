9e7f59e2d08f1b6007b26f852d0c5747c9845b34
KakazuGabriel <142187006+gabrielkakazu@users.noreply.github.com>
2026-05-31 20:18:28 -0300

    clase13 gamma filminas

--- postClase8/clase13.py
+++ postClase8/clase13.py
@@ -1,2 +1,25 @@
 import sqlite3
 
+def crearDB():
+    conexion = sqlite3.connect("alumnos2.db")
+    print("Conexión establecida exitosamente.")
+    conexion.close()
+
+def crearTabla():
+    conexion = sqlite3.connect("alumnos2.db")
+    cursor = conexion.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS parcial1 (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        apellido TEXT NOT NULL,
+        nota FLOAT NOT NULL)
+        ''')
+    conexion.commit()
+    print("Tabla parcial1 creada exitosamente")
+    conexion.close()
+
+if __name__ == "__main__":
+    # crearDB()
+    # crearTabla()
+    pass
+
