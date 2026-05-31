9ed6cdff3fd61b8e6bd9c4d56b2208f1d5507c28
KakazuGabriel <142187006+gabrielkakazu@users.noreply.github.com>
2026-05-31 20:36:23 -0300

    clase 13 gamma  SQLITE3

--- postClase8/clase13.py
+++ postClase8/clase13.py
@@ -18,8 +18,47 @@ def crearTabla():
     print("Tabla parcial1 creada exitosamente")
     conexion.close()
 
+registro = ("Gómez", 9.50)
+
+def insertarRegistro(tupla):
+    conexion = sqlite3.connect("alumnos2.db")
+    cursor = conexion.cursor()
+    cursor.execute('''
+        INSERT INTO parcial1 (apellido, nota)
+        VALUES (?, ?)
+        ''', tupla
+    )
+    conexion.commit()
+    conexion.close()
+    print(f"Registro {tupla} agregado a la tabla")
+
+recuperatorio = 6.5
+id_alumno = 1
+
+def modificarNota(nuevaNota, id):
+    conexion = sqlite3.connect("alumnos2.db")
+    cursor = conexion.cursor()
+    cursor.execute('UPDATE parcial1 SET nota = ? WHERE id = ?', (nuevaNota, id))
+    conexion.commit()
+    conexion.close()
+    print(f"Nota {nuevaNota} actualizada en registro con id {id}")
+
+def borrarRegistro(id_borrar):
+    conexion = sqlite3.connect("alumnos2.db")
+    cursor = conexion.cursor()
+    cursor.execute('DELETE FROM parcial1 WHERE id = ?', (id_borrar,))
+    conexion.commit()
+    conexion.close()
+    print(f"Registro con id {id_borrar} borrado de la tabla")
+
+
+
+
 if __name__ == "__main__":
     # crearDB()
     # crearTabla()
+    # insertarRegistro(registro)
+    # modificarNota(recuperatorio, id_alumno)
+    # borrarRegistro(id_alumno)
     pass
 
